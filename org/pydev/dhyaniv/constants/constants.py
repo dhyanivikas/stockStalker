a3d356f9229304018c94c280b2ddf604e9329733
Vikas A Dhyani <dhyaniv@l-sfff5rlvcg-m.gapinc.gap.com>
2021-01-04 18:50:34 -0800

    changed constants

--- org/pydev/dhyaniv/constants/constants.py
+++ org/pydev/dhyaniv/constants/constants.py
@@ -1,7 +1,8 @@
-IEXENPOINT="https://investors-exchange-iex-trading.p.rapidapi.com/stock/msft/book"
+#IEXENPOINT="https://investors-exchange-iex-trading.p.rapidapi.com/stock/msft/book"
+#IEXENPOINT="https://investors-exchange-iex-trading.p.rapidapi.com/stock/tsla/book"
 APISECRETPATH="../../../../resources/secrets.yaml"
 MSFTALERTPATH="../../../../resources/msft-alert.mp3"
 QUOTE="quote"
 LATESTPRICE="latestPrice"
 OPEN="open"
-CHECKFREQUENCY=20
+CHECKFREQUENCY=45
