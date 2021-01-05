596a60bc582e64d7ecf58ac6edd0878cf12e7ef8
Vikas A Dhyani <dhyaniv@l-sfff5rlvcg-m.gapinc.gap.com>
2021-01-04 20:46:36 -0800

    added logic for harvesting stock open prices

--- org/pydev/dhyaniv/dbfetch/loadStalkerSubjects.py
+++ org/pydev/dhyaniv/dbfetch/loadStalkerSubjects.py
@@ -12,9 +12,28 @@ import org.pydev.dhyaniv.dbfetch.getDBConnection as dbConn
 def getStockSubjectsList(): #no error method
     dbConnection = dbConn.getconnection()
     cur = dbConnection.cursor()
-    cur.execute("select id, stock_symbol, stock_name from stocks_watch_list")
+    cur.execute("select id, stock_symbol, stock_name from stocks_watch_list where track_intra_day = 'Y'")
     stockStalkerSubjects = cur.fetchall()
     return stockStalkerSubjects
 
 
-getStockSubjectsList()
+
+def getOpenPriceSubjectsList():
+    dbConnection = dbConn.getconnection()
+    cur = dbConnection.cursor()
+    cur.execute("select id, stock_symbol, stock_name from stocks_watch_list where track_open_price = 'Y'")
+    openPriceStalkerSubjects = cur.fetchall()
+    return openPriceStalkerSubjects    
+
+#getStockSubjectsList()
+
+def insertIntoOpenPriceTracker(stock_symbol, trading_date, open_price):
+    
+    dbConnection = dbConn.getconnection()
+    cur = dbConnection.cursor()
+    #query = 'INSERT INTO stocks_opening_prices1(stock_symbol, trading_date, open_price) values(%s,%s,%d) '
+    query = 'INSERT INTO stocks_opening_prices(stock_symbol, trading_date, open_price) values(%s,%s,%s) '
+    args = (stock_symbol, trading_date, round(open_price,2))
+    print(stock_symbol+"***"+trading_date+"***"+str(open_price))
+    cur.execute(query, args)
+    dbConnection.commit()
